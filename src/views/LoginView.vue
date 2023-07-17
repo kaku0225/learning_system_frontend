2c9e9fdad735144cb4c51567e195bc01532f74ec
Eason <acontentedmind@gmail.com>
2023-07-17 10:04:20 +0800

    add: LoginView init

---
+++ src/views/LoginView.vue
@@ -0,0 +1,80 @@
+<template>
+  <!-- Section: Design Block -->
+  <section class="text-center text-lg-start">
+
+    <!-- Jumbotron -->
+    <div class="container py-4">
+      <div class="row g-0 align-items-center">
+        <div class="col-lg-6 mb-5 mb-lg-0">
+          <div class="card cascading-right" style="
+              background: hsla(0, 0%, 100%, 0.55);
+              backdrop-filter: blur(30px);
+              ">
+            <div class="card-body p-5 shadow-5 text-center">
+              <h2 class="fw-bold mb-5">Login now</h2>
+              <form>
+                <!-- Email input -->
+                <div class="d-flex justify-content-center">
+                  <div class="form-outline mb-4 w-75">
+                    <input type="email" id="loginEmail" class="form-control" placeholder="Email"/>
+                  </div>
+                </div>
+
+                <!-- Password input -->
+                <div class="d-flex justify-content-center">
+                  <div class="form-outline mb-4 w-75">
+                    <input type="password" id="loginPassword" class="form-control" placeholder="Password"/>
+                  </div>
+                </div>
+
+                <!-- Submit button -->
+                <button type="submit" class="btn btn-primary btn-block mb-4">
+                  Sign up
+                </button>
+
+                <!-- Register buttons -->
+                <div class="text-center">
+                  <p>or login with:</p>
+                  <button type="button" class="btn btn-link btn-floating mx-1">
+                    <i class="fab fa-facebook-f"></i>
+                  </button>
+
+                  <button type="button" class="btn btn-link btn-floating mx-1">
+                    <i class="fab fa-google"></i>
+                  </button>
+
+                  <button type="button" class="btn btn-link btn-floating mx-1">
+                    <i class="fab fa-twitter"></i>
+                  </button>
+
+                  <button type="button" class="btn btn-link btn-floating mx-1">
+                    <i class="fab fa-github"></i>
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-6 mb-5 mb-lg-0">
+          <img src="https://mdbootstrap.com/img/new/ecommerce/vertical/004.jpg" class="w-100 rounded-4 shadow-4"
+            alt="" />
+        </div>
+      </div>
+    </div>
+    <!-- Jumbotron -->
+  </section>
+  <!-- Section: Design Block -->
+</template>
+
+<style>
+.cascading-right {
+  margin-right: -50px;
+}
+
+@media (max-width: 991.98px) {
+  .cascading-right {
+    margin-right: 0;
+  }
+}
+</style>
