5ddd0fe32868da13e2a78be92e9350063594c5e4
Eason <acontentedmind@gmail.com>
2023-07-24 13:34:20 +0800

    Init: graphql-codegen

---
+++ codegen.ts
@@ -0,0 +1,16 @@
+import { CodegenConfig } from '@graphql-codegen/cli'
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: "http://localhost:3000/graphql",
+  documents: 'src/graphql/**/*',
+  ignoreNoDocuments: true,
+  generates: {
+    './src/gql/': {
+      preset: 'client',
+      plugins: ['typescript', 'typescript-operations'],
+    }
+  }
+}
+
+export default config
