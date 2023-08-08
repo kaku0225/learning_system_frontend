960cc60c44432ea15250c00e195b79ecb9efcb61
Eason <acontentedmind@gmail.com>
2023-08-08 09:16:04 +0800

    Feat: logout a user after 10 minutes of inactivity

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,4 @@
 <script setup>
-  import { watchEffect, onMounted } from 'vue'
-  import { useQuery } from '@vue/apollo-composable';
-  import gql from 'graphql-tag'
-  import router from './router'
-
-  const token = document.cookie.replace(/(?:(?:^|.*;\s*)token\s*=\s*([^;]*).*$)|^.*$/, '$1');
-  function checkLogin() {
-    const { result } = useQuery(
-      gql`
-        query CheckLogin($token: String!) {
-          checkLogin(token: $token)
-        }
-      `,
-      {
-        token: token,
-      }
-    );
-    watchEffect(() => {
-      if (result.value && result.value.checkLogin !== true) {
-        router.push('/login')
-      }
-    })
-  }
-
-  onMounted(() => {
-    checkLogin()
-  })
 </script>
 
 <template>
--- src/layouts/WithNav.vue
+++ src/layouts/WithNav.vue
@@ -1,6 +1,70 @@
 <script setup>
 import Sidebar from '../components/SidebarsView.vue'
 import Navbars from '../components/NavbarsView.vue'
+import { watchEffect, onMounted } from 'vue'
+import { useMutation, useQuery } from '@vue/apollo-composable';
+import gql from 'graphql-tag'
+import router from '../router'
+
+const token = document.cookie.replace(/(?:(?:^|.*;\s*)token\s*=\s*([^;]*).*$)|^.*$/, '$1');
+
+const userActivityTimeout = 10 * 60 * 1000; // 10 minutes in milliseconds
+
+let timeoutId
+
+const { mutate: logout } = useMutation(gql`
+  mutation logout ($token: String!) {
+    logout (input: {token: $token }) {
+      expiredTime
+    }
+  }
+`, () => ({
+  variables: {
+    token: token,
+  },
+}))
+
+const handleUserActivity = () => {
+  clearTimeout(timeoutId);
+  timeoutId = setTimeout(mutationLogout, userActivityTimeout);
+};
+
+function mutationLogout(){
+  logout().then(result => {
+    clearTimeout(timeoutId);
+    window.removeEventListener('mousemove', handleUserActivity);
+    window.removeEventListener('keydown', handleUserActivity);
+    const expired_time = result.data.logout.expiredTime
+    document.cookie = `token=null; expires=${new Date(expired_time)}`;
+    router.push('/login')
+  });
+}
+
+function checkLogin() {
+  const { result } = useQuery(
+    gql`
+      query CheckLogin($token: String!) {
+        checkLogin(token: $token)
+      }
+    `,
+    {
+      token: token,
+    }
+  );
+  watchEffect(() => {
+    if (result.value && result.value.checkLogin === false) {
+      router.push('/login')
+    }
+  })
+}
+
+onMounted(() => {
+  checkLogin()
+
+  timeoutId = setTimeout(mutationLogout, userActivityTimeout);
+  window.addEventListener('mousemove', handleUserActivity);
+  window.addEventListener('keydown', handleUserActivity);
+})
 </script>
 
 <template>
